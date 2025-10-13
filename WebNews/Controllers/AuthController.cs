739f2afec2e9b51808f80ee7ff7c8d2ce5814761
51dollar <borushkoddima@mail.ru>
2025-10-13 23:50:17 +0300

    add base logic login and logout

--- WebNews/Controllers/AuthController.cs
+++ WebNews/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using WebNews.Helpers;
 using WebNews.Models;
 using WebNews.Models.ViewModels;
 using WebNews.Services;
-using WebNews.Services.Interfaces;
 
 namespace WebNews.Controllers;
 
@@ -46,7 +48,67 @@ public class AuthController : Controller
             userViewModel.User.PasswordHash
         );
 
-        await _service.CreateAsync(userViewModel.User);
+        User user = new User()
+        {
+            Username = userViewModel.User.Username,
+            Email = userViewModel.User.Email,
+            PasswordHash = userViewModel.User.PasswordHash
+        };
+
+        await _service.CreateAsync(user);
+        return RedirectToAction("Home");
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(UserViewModel userViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            var userFromDb = await _service.GetUserByEmailAsync(userViewModel.User.Email);
+            if (userFromDb == null)
+            {
+                ViewBag.Error = "Email doesn't exist";
+                return View(userViewModel);
+            }
+
+            userViewModel.User.PasswordHash = _hasher.HashPassword(
+                userViewModel.User,
+                userViewModel.User.PasswordHash
+            );
+
+            if (userViewModel.User.PasswordHash.Equals(userFromDb.PasswordHash))
+            {
+                ViewBag.Error = "Passwords no match";
+                return View(userViewModel);
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userViewModel.User.Id.ToString()),
+                new Claim(ClaimTypes.Email, userViewModel.User.Username),
+                new Claim(ClaimTypes.Name, userViewModel.User.Email),
+                new Claim(ClaimTypes.Role, userViewModel.User.Role),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
+        }
+
+        return RedirectToAction("Home");
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         return RedirectToAction("Home");
     }
 }
